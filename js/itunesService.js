9e7a036c8a99fac96a555b61cd5fd589d1876c01
Nick Marucci <npm1514@gmail.com>
2015-12-17 17:14:58 -0700

    Completion

--- js/itunesService.js
+++ js/itunesService.js
@@ -1,14 +1,29 @@
 var app = angular.module('itunes');
 
-app.service('itunesService', function($http, $q){
-  //This service is what will do the 'heavy lifting' and get our data from the iTunes API.
-  //Also note that we're using a 'service' and not a 'factory' so all your methods you want to call in your controller need to be on 'this'.
+app.service('itunesService', function($http, $q) {
 
-  //Write a method that accepts an artist's name as the parameter, then makes a 'JSONP' http request to a url that looks like this
-  //https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK'
-  //Note that in the above line, artist is the parameter being passed in. 
-  //You can return the http request or you can make your own promise in order to manipulate the data before you resolve it.
-
-    //Code here
-    
+  this.getArtist = function(artist) {
+    var deferrer = $q.defer();
+    $http({
+      method: 'JSONP',
+      url: "https://itunes.apple.com/search?term=" + artist + "&callback=JSON_CALLBACK"
+    }).then(function(response) {
+      if (response) {
+        deferrer.resolve(response.data.results);
+      } else {
+        deferrer.reject(response.data.results);
+        alert("oops!  Something went wrong.");
+      }
+    });
+    return deferrer.promise;
+  };
 });
+//This service is what will do the 'heavy lifting' and get our data from the iTunes API.
+//Also note that we're using a 'service' and not a 'factory' so all your methods you want to call in your controller need to be on 'this'.
+
+//Write a method that accepts an artist's name as the parameter, then makes a 'JSONP' http request to a url that looks like this
+//https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK'
+//Note that in the above line, artist is the parameter being passed in.
+//You can return the http request or you can make your own promise in order to manipulate the data before you resolve it.
+
+//Code here
